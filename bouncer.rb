53673d91237b9096387421b10905335c1d4aab2e
Xavi G <alexandro.x.guerrero@gmail.com>
2015-09-09 09:00:40 -0500

    fixed the concatenation issue

--- bouncer.rb
+++ bouncer.rb
@@ -2,8 +2,16 @@ def check_age age_input
   if age_input < 18
     puts "You can do NOTHING, little one."
   elsif age_input >= 18 && age_input < 21
-    puts "You can smoke AND vote; how nice, little one."
+    young_adult = "You can smoke AND vote; how nice, little one."
+    puts young_adult
   elsif age_input >= 21 && age_input < 25
+    slightly_older_young_adult = "#{young_adult} Oh, and drink as well? Perhaps not so little."
+    puts slightly_older_young_adult
+  else
+    ancient_young_adult = slightly_older_young_adult + " Ooooh, even rent a car! You're practically dead!"
+    puts ancient_young_adult
+  end
+end
 
 
 
@@ -11,6 +19,6 @@ puts "Step into my parlor."
 puts "Now tell me."
 puts "How old are you? You have years of age, yes?"
 
-age_answer = gets.chomp
+age_answer = gets.chomp.to_i
 
 check_age age_answer
